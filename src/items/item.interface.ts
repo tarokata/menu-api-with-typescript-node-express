5d042a467c9cd847d468d6000cb121f6785cf6da
ThienHung-COWELL <hungthinh.work@gmail.com>
2021-07-13 14:20:43 +0700

    First commit

---
+++ src/items/item.interface.ts
@@ -0,0 +1,13 @@
+// src/items/item.interface.ts
+
+export interface BaseItem {
+    name: string;
+    price: number;
+    description: string;
+    image: string;
+}
+
+export interface Item extends BaseItem {
+    id: number;
+}
+
